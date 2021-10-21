0b781218101b9e241c8dda53203b3346998daa46
rutujaburkul264 <rutujaburkul264@gmail.com>
2021-10-21 22:42:04 +0530

    Given a Message, ability

--- UC1_MoodAnalyser.cs
+++ UC1_MoodAnalyser.cs
@@ -1,1 +1,18 @@
-Welcome
+using System;
+using System.Text;
+
+namespace MoodAnalyserAssignment
+{
+    public class UC1_MoodAnalyser
+    {
+        private string message;
+        public UC1_MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public void UC1_MoodAnalysers1()
+        {
+        }
+    }
+}
+
